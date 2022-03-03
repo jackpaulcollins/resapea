6199fa53e2039a2db3fd7ae9e7441f5c5eaa3b2c
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-02 22:23:38 -0800

    bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,12 +5,12 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     params = {
-      api_key => ENV['SMTP_KEY'],
-      to => [@user],
-      sender => "jack@resapea.io",
-      subject => "Hello Beautifu",
-      text_body => "You're my favorite person ever",
-      html_body => "<h1>You're my favorite person ever</h1>",
+      api_key: ENV['SMTP_KEY'],
+      to: [@user],
+      sender: "jack@resapea.io",
+      subject: "Hello Beautifu",
+      text_body: "You're my favorite person ever",
+      html_body: "<h1>You're my favorite person ever</h1>",
   }
     encoded_params = URI.encode_www_form(params)
     response = Faraday.post("https://api.smtp2go.com/v3/email/send", encoded_params)
