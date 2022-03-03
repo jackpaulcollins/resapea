919ad6c803fe3c39235e88bd762fe21dd720870c
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-02 22:18:34 -0800

    add faraday

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,19 @@
 class UserMailer < ActionMailer::Base
-  default from: "jack@resapea.io"
+  require "faraday"
+  require 'uri'
+
   def welcome_email(user)
-      @user = user
-      mail(to: @user.email, subject: 'Welcome to Resapea!')
-    end
+    @user = user
+    params = {
+      api_key => ENV['SMTP_PW'],
+      to => [@user],
+      sender => "jack@resapea.io",
+      subject => "Hello Beautifu",
+      text_body => "You're my favorite person ever",
+      html_body => "<h1>You're my favorite person ever</h1>",
+  }
+    encoded_params = URI.encode_www_form(params)
+    response = Faraday.post("https://api.smtp2go.com/v3/email/send", encoded_params)
+    response.body if response.status == 201
   end
+end
