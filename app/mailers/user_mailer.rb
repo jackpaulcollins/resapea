a936d654566f73ca2fa4b93bb56c61e40e237975
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-03 17:31:22 -0800

    change domain

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ActionMailer::Base
+  default from: "jack@respea.io"
   def welcome_email(user)
       @user = user
       mail(to: @user.email, subject: 'Welcome to Resapea!')
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   :authentication => :plain,
   :address => "smtp.mailgun.org",
   :port => 587,
-  :domain => "resapea.mailgun.org",
+  :domain => "mail.resapea.io",
   user_name: Rails.application.credentials.mailgun[:user_name],
   password: Rails.application.credentials.mailgun[:password]
 }
