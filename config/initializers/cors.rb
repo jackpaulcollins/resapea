600590fb974e64a07eda9e8aee2221bb7513fcb2
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-05 23:38:02 -0800

    add resapea to cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://friendly-keller-cdf4af.netlify.app"
+    origins "https://www.resapea.io/"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
