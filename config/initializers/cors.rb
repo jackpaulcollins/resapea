87f6dfd5bf6846176c6d9d78f0870fa9e68372c6
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-06 00:00:12 -0800

    bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://www.resapea.io/"
+    origins "https://www.resapea.io"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
