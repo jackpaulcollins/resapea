87e365dadbb21f3a9cdf865e30b664b0d12db24d
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-03-06 18:22:10 -0800

    add is_admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_035049) do
+ActiveRecord::Schema.define(version: 2022_03_07_022033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_035049) do
     t.string "username"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.boolean "is_admin"
   end
 
 end
